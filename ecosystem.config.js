e2e8289dff1858be79ed2f7998a850eb89f5a257
arthurzhang262 <arthur262@outlook.com>
2025-08-19 15:35:19 +0800

    azure fix

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	apps: [
+		{
+			name: "boletio",
+			script: "node_modules/next/dist/bin/next",
+			args: "start",
+			env: {
+				PORT: process.env.PORT || 3000,
+			},
+			watch: false,
+			autorestart: true,
+		},
+	],
+};
+
+
